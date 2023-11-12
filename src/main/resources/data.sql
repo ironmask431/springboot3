e489e89b0ce8010f55d1ffa955f11598e7b994af
ironmask431 <ironmask431@gmail.com>
2023-11-12 18:25:51 +0900

    oauth 10.5 BlogApiControllerTest 테스트코드 오류 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 INSERT INTO USERS(email, password) VALUES ('test@naver.com','$2a$10$UTdnBjWGm.MOGSCiA8wnxeGda1BEaaIC/1S74yabcoRIDvMt7ze/K');
 
-INSERT INTO article (title, content, author, created_at, updated_at) VALUES ('제목1','내용1', 'user1', NOW(), NOW());
-INSERT INTO article (title, content, author, created_at, updated_at) VALUES ('제목2','내용2', 'user2', NOW(), NOW());
-INSERT INTO article (title, content, author, created_at, updated_at) VALUES ('제목3','내용3', 'user3', NOW(), NOW());
+INSERT INTO article (title, content, author, created_at, updated_at) VALUES ('안녕하세요','반갑습니다', 'user1', NOW(), NOW());
+INSERT INTO article (title, content, author, created_at, updated_at) VALUES ('사랑합니다','사랑둥이떠', 'user2', NOW(), NOW());
+INSERT INTO article (title, content, author, created_at, updated_at) VALUES ('좋아합니다.','귀요우미떠어', 'user3', NOW(), NOW());
--- src/test/java/kevin/study/springboot3/blog/controller/BlogApiControllerTest.java
+++ src/test/java/kevin/study/springboot3/blog/controller/BlogApiControllerTest.java
@@ -4,14 +4,20 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import kevin.study.springboot3.blog.domain.Article;
 import kevin.study.springboot3.blog.dto.ArticleRequest;
 import kevin.study.springboot3.blog.repository.BlogRepository;
+import kevin.study.springboot3.user.domain.User;
+import kevin.study.springboot3.user.repository.UserRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -19,8 +25,10 @@ import org.springframework.web.context.WebApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.security.Principal;
 import java.util.List;
 
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -41,6 +49,14 @@ class BlogApiControllerTest {
     @Autowired
     private BlogRepository blogRepository;
 
+    @Autowired
+    private UserRepository userRepository;
+
+    @Mock
+    Principal principal;
+
+    User user;
+
     @BeforeEach
     void mockMvcSetup() {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
@@ -48,11 +64,23 @@ class BlogApiControllerTest {
         blogRepository.deleteAll();
     }
 
+    @BeforeEach
+    void setSecurityContext(){
+        userRepository.deleteAll();
+        user = userRepository.save(User.builder()
+                                       .email("user@gmail.com")
+                                       .password("test")
+                                       .build());
+
+        SecurityContext context = SecurityContextHolder.getContext();
+        context.setAuthentication(new UsernamePasswordAuthenticationToken(user, user.getPassword(), user.getAuthorities()));
+    }
+
     @Test
     @DisplayName("블로그 글 추가 api 테스트")
     void addArticleTest() throws Exception {
         //given
-        final String url = "/api/article";
+        final String url = "/api/articles";
         final String title = "제목";
         final String content = "내용";
         final ArticleRequest request = ArticleRequest.builder()
@@ -63,9 +91,12 @@ class BlogApiControllerTest {
         //request 객체를 String (JSON 형태)으로 직렬화
         final String requestBody = objectMapper.writeValueAsString(request);
 
+        when(principal.getName()).thenReturn("username");
+
         //when
         ResultActions result = mockMvc.perform(post(url)
                 .contentType(MediaType.APPLICATION_JSON)
+                .principal(principal)
                 .content(requestBody));
 
         //then
@@ -85,9 +116,8 @@ class BlogApiControllerTest {
         final String url = "/api/articles";
         final String title = "제목";
         final String content = "내용";
-        final String author = "user1";
 
-        createSavedArticle(title, content, author);
+        createSavedArticle(title, content);
 
         //when & then
         mockMvc.perform(get(url).accept(MediaType.APPLICATION_JSON))
@@ -105,9 +135,8 @@ class BlogApiControllerTest {
 
         final String title = "제목";
         final String content = "내용";
-        final String author = "user1";
 
-        final Article savedArticle = createSavedArticle(title, content, author);
+        final Article savedArticle = createSavedArticle(title, content);
 
         //when
         ResultActions result = mockMvc.perform(get(url, savedArticle.getId()));
@@ -122,10 +151,10 @@ class BlogApiControllerTest {
     @DisplayName("블로그 단일글 삭제 api 테스트")
     void deleteArticleTest() throws Exception {
         //given
-        final String url = "/api/article/{id}";
+        final String url = "/api/articles/{id}";
         //pathVariable 형태의 URL도 아래와 mockMvc.perform() 에서 사용 가능
 
-        Article savedArticle = createSavedArticle("제목", "내용","user1");
+        Article savedArticle = createSavedArticle("제목", "내용");
 
         Long articleId = savedArticle.getId();
         //when
@@ -142,9 +171,9 @@ class BlogApiControllerTest {
     @DisplayName("블로그 글 수정 api 테스트")
     void updateArticleTest() throws Exception {
         //given
-        Article savedArticle = createSavedArticle("제목", "내용","user1");
+        Article savedArticle = createSavedArticle("제목", "내용");
 
-        final String url = "/api/article/{id}";
+        final String url = "/api/articles/{id}";
         final String title = "수정된 제목";
         final String content = "수정된 내용";
 
@@ -170,11 +199,11 @@ class BlogApiControllerTest {
         assertThat(articles.getContent()).isEqualTo(content);
     }
 
-    private Article createSavedArticle(String title, String content, String author) {
+    private Article createSavedArticle(String title, String content) {
         Article article = Article.builder()
                                  .title(title)
                                  .content(content)
-                                 .author(author)
+                                 .author(user.getUsername())
                                  .build();
         return blogRepository.save(article);
     }
